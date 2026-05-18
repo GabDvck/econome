ac5a3c799d25ce1b8c54f1295bcebdf98c8a07e0
devick241 <gabriel.devick@etu.unicaen.fr>
2026-05-18 17:36:05 +0200

    modif afficherTransactions + création majSolde()

--- js/dom.js
+++ js/dom.js
@@ -1,21 +1,26 @@
-function afficherTransactions(liste){
-    const tbody = document.createElement("tbody");
-    tbody.classList.add("liste-transactions");
+const liste_transactions = document.getElementById("liste-transactions");
+const solde_html = document.getElementById("solde-montant");
+
+export function afficherTransactions(liste){
+    liste_transactions.innerHTML = '';
     for (let transac of liste){
         const tr = document.createElement("tr");
-        const date = document.createElement("td"); date.innerHTML = trasac["date"]; tr.appendChild(date);
-        const description = document.createElement("td"); description.innerHTML = transac["description"]; tr.appendChild(description);
-        const categorie = document.createElement("td"); categorie.innerHTML = transac["categorie"]; tr.appendChild(categorie);
+        const date = document.createElement("td"); date.textContent = trasac["date"]; tr.appendChild(date);
+        const description = document.createElement("td"); description.textContent = transac["description"]; tr.appendChild(description);
+        const categorie = document.createElement("td"); categorie.textContent = transac["categorie"]; tr.appendChild(categorie);
         const montant = document.createElement("td");
         const stringMontant = transac["montantEuros"].toString().replace(".", ",") + "," + transac["montantCentimes"].toString();
         if (transac["montantEuros"] > 0){
-            montant.innerHTML = "+€" + stringMontant;
+            montant.textContent = "+€" + stringMontant;
         }
         else{
-            montant.innerHTML = "-€" + stringMontant;
+            montant.textContent = "-€" + stringMontant;
         }
         tr.appendChild(montant);
-        tbody.appendChild(tr);
+        liste_transactions.appendChild(tr);
     }
-    return tbody;
+}
+
+export function majSolde(solde){
+    solde_html.textContent = `€${total.toFixed(2)}`;
 }
